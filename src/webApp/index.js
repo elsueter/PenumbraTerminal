526e7592507848b82ebe4ae3fda5d64ead81fecc
Joseph Straw <joeojejoe123@hotmail.co.uk>
2021-07-12 17:44:59 +0100

    sanitised inputs

--- src/webApp/index.js
+++ src/webApp/index.js
@@ -5,49 +5,93 @@ const term = new Terminal({
     fontFamily: 'Courier New',
     });
     
-term.resize(80, 50);
+term.resize(80, 40);
 term.open(document.getElementById('terminal'));
 
-var currLine = "";
-var entries = [];
+let currLine = "";
+let entries = [];
+let entriesIndex = 0;
 
-term.writeSlow = (line, index) => {
-    term.write(line.charAt(index));
-    if(index < line.length-1){
-        setTimeout(function(){term.writeSlow(line, index+1)}, 0);
-    }else{
-        term.write("\r\n");
-        term.writeSlowNR("\x1b[1;34mPENUMBRA OS \x1b[1;32m$ ", 0)
-    }
+let printing = false;
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-term.writeSlowNR = (line, index) => {
-    term.write(line.charAt(index));
-    if(index < line.length-1){
-        setTimeout(function(){term.writeSlowNR(line, index+1)}, 0);
-    }
+async function prompt(){
+    entriesIndex = 0;
+    term.write("\r\n");
+    line = "\x1b[1;34mPENUMBRA OS \x1b[1;32m$ ";
+    writeSlow(line, false);
 }
 
-term.writeSlowNR("\x1b[1;32m================================================================================\r\n\x1b[1;34mPENUMBRA OS V 1.0.1.6\r\n\x1b[1;32m================================================================================\r\n\x1b[1;34minitialise . . . . : true\r\n\x1b[1;34mup to date . . . . : true\r\n\r\n\x1b[1;34mPENUMBRA OS \x1b[1;32m$ ", 0)
+async function writeSlow (line, nl){
+    printing = true;
+    for(index = 0; index < line.length; index++){
+        term.write(line.charAt(index));
+        await sleep(0);
+    }
+    if(nl){
+        prompt();
+    }
+    printing = false;
+}
+
+writeSlow("\x1b[1;32m================================================================================\r\n\x1b[1;34mPENUMBRA OS V 1.0.1.6\r\n\x1b[1;32m================================================================================\r\n\x1b[1;34minitialise . . . . : true\r\n\x1b[1;34mup to date . . . . : true\r\n", true)
 
 term.onKey((key, ev) => {
-    if(key.key == "\r"){
-        if(currLine){
-            entries.push(currLine);
-            fetchData(currLine);
-            currLine = "";
-            }else{
-            term.write("\r\n");
-            term.writeSlowNR("\x1b[1;34mPENUMBRA OS \x1b[1;32m$ ", 0)
+    if(!printing){
+        if(key.domEvent.code == "Backspace"){
+            if(currLine){
+                currLine = currLine.substring(0, currLine.length - 1);
+                term.write("\b \b")
+            }
+        }else{
+            switch(key.key){
+                case "\r":
+                    if(currLine){
+                        entries.push(currLine);
+                        fetchData(currLine);
+                        currLine = "";
+                    }else{
+                        prompt();
+                    }
+                    break;
+                case "\u001b[A":
+                    if(entriesIndex != entries.length){
+                        entriesIndex += 1;
+                    }
+                    for(let i = 0; i < currLine.length; i++){
+                        term.write("\b \b")
+                    }
+                    writeSlow(entries[entries.length-entriesIndex], false);
+                    currLine = entries[entries.length-entriesIndex];
+                    break;
+                case "\u001b[B":
+                    for(let i = 0; i < currLine.length; i++){
+                        term.write("\b \b")
+                    }
+                    if(entriesIndex != 1){
+                        entriesIndex -= 1;
+                        writeSlow(entries[entries.length-entriesIndex], false);
+                        currLine = entries[entries.length-entriesIndex];
+                    }else{
+                        if(entriesIndex != 0){
+                            entriesIndex -= 1;
+                        }
+                        currLine = "";
+                    }
+                    break;
+                case "\u001b[C":
+                    break;
+                case "\u001b[D":
+                    break;
+                default:
+                    currLine += key.key;
+                    term.write(key.key);
+                    break;
+            }
         }
-    }else if(key.domEvent.code == "Backspace"){
-        if(currLine){
-            currLine = currLine.substring(0, currLine.length - 1);
-            term.write("\b \b")
-        }
-    }else{
-        currLine += key.key;
-        term.write(key.key);
     }
 });
 
@@ -55,7 +99,7 @@ function fetchData(line) {
     data = JSON.stringify(line);
     console.log(data);
 
-    fetch('penumbraNetwork/fetch', {
+    fetch('fetch', {
                 method: 'POST',
                 body: data,
             })
@@ -75,7 +119,7 @@ function fetchData(line) {
             .then( myJson =>
             {
                 console.log(myJson);
-                term.writeSlow(myJson.data, 0);
+                writeSlow(myJson.data, true);
             } )
             .catch( err =>
             {
